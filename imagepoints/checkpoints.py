0dac96779b10d2b6194b1d2045415e19b97f72a0
Elyssa McMaster <elyssamcmaster@gmail.com>
2022-11-11 11:53:52 +0100

    changed folder name to markpoints

--- imagepoints/checkpoints.py
+++ imagepoints/checkpoints.py
@@ -4,7 +4,6 @@ image = plt.imread("") ## put the path to your image here
 pts = np.array([[357,782],[479,1003],[678, 494],[204,607]]) ## the points here were mashed into the computer completely randomly and should be adjusted for the points you wish to see on your image
 
 print(image.size)
-plt.imshow(image)
-plt.plot(640, 570, "og", markersize=5) 
+plt.imshow(image) 
 plt.scatter(pts[:, 0], pts[:, 1], marker="x", color="red", s=20)
 plt.show()
